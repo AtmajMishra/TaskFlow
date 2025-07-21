2888dc9bc4564a16c596e31d846d2b46591b4657
Atmaj Mishra <mishraatmaj@gmail.com>
2025-07-21 22:26:57 +0530

    changes in the auth.js file

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,4 +12,26 @@ export default async function authMiddleware(req,res,next)
         return res.status(401).json({success:false, message:"Not Authorized, token missing"})
     }
 
+    const token= authHeader.split('')[1];
+
+    // VERIFY AND ATTACH USER OBJECT
+
+    try{
+        const payload=jwt.verify(token, JWT_SECRET);
+        const user=await User.findById(payload.id).select('-password');
+
+        if(!user)
+        {
+            return res.status(401).json({success:false, message:"User not found"});
+
+        }
+        req.user=user;
+        next();
+
+    }catch(error){
+        console.log("JWT verification failed", err);
+        return res.status(401).json({success:false, message:"Token invalid or expired" })
+
+    }
+
 }
