517ab345afeeed2514a511c508a5f3bebc3881d5
Atmaj Mishra <mishraatmaj@gmail.com>
2025-07-28 22:13:43 +0530

    Navbar in progress

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { use } from "react";
+import { useNavigate } from "react-router-dom";
+import { Zap } from "lucide-react";
 
 const Navbar = () => {
-    return ( <div>Navbar</div>)
-}
+  const navigate = useNavigate();
+  return (
+    <header className=" sticky top-0 z-50 bg-white/90 shadow-sm backdrop-blur-md border-b border-gray-200 font-sans">
+      <div className=" flex items-center justify-between px-4 py-3 md:px-6 max-w-7xl mx-auto">
+        {/* Logo can be added here */}
+        <div
+          className=" flex items-center gap-2 cursor-pointer group"
+          onClick={() => navigate("/")} >
+          {/*LOGO*/}
+          <div
+            className="relative w-10 h-10 flex items-center justify-center rounded-xl 
+            bg-gradient-to-br from-fuchsia-500 
+            via-purple-500 to-indigo-500 shadow-lg group-hover:shadow-purple-300/50 
+             group-hover:scale-105 transition-all duration-300 ">
+            {/**/ }
+            <Zap className="w-6 h-6 text-white" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
